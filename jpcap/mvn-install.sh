4d4c14586067671fb6988e135d67a3f2b7d3e9f9
Matt Harvey <matt@teamdawg.org>
2013-12-01 14:55:12 -0800

    rebuild jpcap

--- jpcap/mvn-install.sh
+++ jpcap/mvn-install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-mvn install:install-file -Dfile=0.7/jpcap.jar -DgroupId=net.sourceforge.jpcap -DartifactId=jpcap -Dversion=0.7.0 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=0.01.18/jpcap.jar -DgroupId=net.sourceforge.jpcap -DartifactId=jpcap -Dversion=0.01.18 -Dpackaging=jar -DgeneratePom=true
